8e33be0f8ac0a4f8c5c0d791ea5572e7967d10c2
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2018-09-15 15:34:33 +0800

    [solidity] add refund contract

---
+++ solidity/src/refund.sol
@@ -0,0 +1,25 @@
+// 接受用户转入指定价值的eth，如果用户转入eth较少，则直接返回异常，当用户转入eth较多，则将用户多转入的eth退回
+
+contract Refund {
+    
+    address owner = 0x0;
+  
+    uint256 ticket = 1 ether;    // 一个eth
+      
+    // 合约构造函数
+    // 第一次部署合约时，会调用该方法。
+    // 之后执行合约不会调用。
+    function Refund() public payable {
+        // 将部署合约的地址作为合约拥有者
+        owner = msg.sender;
+    }
+  
+    // 后备函数
+    function () public payable {
+          require(msg.value >= ticket);
+          if (msg.value > ticket) {
+               var refundFee = msg.value - ticket;
+            msg.sender.transfer(refundFee);
+        }
+    }
+}
