9a3433eab9cf66efed5ddbcd88815abe898f1a24
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-11 23:27:49 +0800

    update

--- solidity/inbox_project/deploy.js
+++ solidity/inbox_project/deploy.js
@@ -1,20 +1,27 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode} = require('./compile');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
 
 const provider = new HDWalletProvider(
-    'resist okay grid decide subject half there will lady size turkey cheap',
-'https://rinkeby.infura.io/asdfghjklqwertyuiiio'
+  "resist okay grid decide subject half there will lady size turkey cheap",
+  "https://rinkeby.infura.io/v3/c627a81327cb4a89bbe4289a399ae6b9"
 );
 
-const web = new Web3(provider);
+// const provider = new HDWalletProvider(
+//     'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
+//     'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+//   );
+const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
-    console.log('attemping to deploy from account', accounts[0]);
-    // console.log(accounts);
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data:bytecode, arguments:['Hi there!']})
-    .send({gas: '1000000',from:accounts[0]});
-    console.log('Contract deployed to ', result.options.address);
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
 };
+deploy();
