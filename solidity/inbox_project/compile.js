1f9ba4598f54b27e3497b3ef0512b6d43672c364
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-12 11:39:25 +0800

    update compile.js

--- solidity/inbox_project/compile.js
+++ solidity/inbox_project/compile.js
@@ -1,12 +1,12 @@
 // require('./contracts/inbox.sol')
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
 
-const  inboxPath = path.resolve(__dirname,'contracts','inbox.sol');
-const source = fs.readFileSync(inboxPath,'utf8');
+const inboxPath = path.resolve(__dirname, "contracts", "inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 
-console.log(solc.compile(source, 1));
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+// console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
 
 /* contracts:inbox:bytecode, interface(ABI)*/
