d99562120829ca1d9f3c9833443b0db42f333a06
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-12 15:21:26 +0800

    update deploy,js

--- solidity/inbox_project/deploy.js
+++ solidity/inbox_project/deploy.js
@@ -7,21 +7,17 @@ const provider = new HDWalletProvider(
   "https://rinkeby.infura.io/v3/c627a81327cb4a89bbe4289a399ae6b9"
 );
 
-// const provider = new HDWalletProvider(
-//     'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
-//     'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
-//   );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-
   console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: "0x" + bytecode, arguments: ["Hi there!"] })
     .send({ gas: "1000000", from: accounts[0] });
 
   console.log("Contract deployed to", result.options.address);
 };
+
 deploy();
